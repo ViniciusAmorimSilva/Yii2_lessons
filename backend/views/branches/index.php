1a9ffdc47ba48be7f23a9d3d930b2ec040905346
Vinicius <amorimm.vinicius@gmail.com>
2018-11-15 16:07:44 -0200

    Searching Related Table From the GridView

--- backend/views/branches/index.php
+++ backend/views/branches/index.php
@@ -24,10 +24,13 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
+            [
+                'attribute'=>'companies_company_id',
+                'value'=>'companiesCompany.company_name',
+            ],
             //'branch_id',
             //'companies_company_id',
-            'companiesCompany.company_name',
+            //'companiesCompany.company_name',
             'branch_name',
             'branch_address',
             'branch_created_date',
